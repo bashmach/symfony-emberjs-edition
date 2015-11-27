f3453de4a67591acb96f6aa2fa953063cf6e8e1b
Pavel Machekhin <pavel.machekhin@gmail.com>
2015-11-27 18:30:16 +0200

    Add routes for categories and products

---
+++ src/Acme/EmberBundle/Resources/public/app/adapters/application.js
@@ -0,0 +1,17 @@
+import DS from 'ember-data';
+import config from '../config/environment';
+
+export default DS.RESTAdapter.extend({
+  namespace: config.APP.namespace,
+  host: config.APP.host,
+
+  shouldReloadAll: function() {
+    return true;
+  },
+  shouldBackgroundReloadAll: function() {
+    return false;
+  },
+  shouldBackgroundReloadRecord: function() {
+    return false;
+  }
+});
--- src/Acme/EmberBundle/Resources/public/app/router.js
+++ src/Acme/EmberBundle/Resources/public/app/router.js
@@ -6,6 +6,12 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.resource('categories', function() {
+    this.resource('category', { path: ':id' });
+  });
+  this.resource('products', function() {
+    this.resource('product', { path: ':id' });
+  });
 });
 
 export default Router;
