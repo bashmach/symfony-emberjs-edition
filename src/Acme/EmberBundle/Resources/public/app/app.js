31c83f9cfa1b3a1de8610c56e48ddf87e24d3528
Pavel Machekhin <pavel.machekhin@gmail.com>
2015-12-08 12:50:33 +0200

    Serve ember application through symfony routes

--- src/Acme/EmberBundle/Resources/public/app/app.js
+++ src/Acme/EmberBundle/Resources/public/app/app.js
@@ -8,6 +8,8 @@ let App;
 Ember.MODEL_FACTORY_INJECTIONS = true;
 
 App = Ember.Application.extend({
+  rootElement: '#app',
+
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
   Resolver
