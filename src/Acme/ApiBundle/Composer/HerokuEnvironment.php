bd0a9bd3144f341770ac76a131b91cd2dbe1328c
Pavel Machekhin <pavel.machekhin@gmail.com>
2016-01-05 11:58:20 +0200

    Set database driver for heroku environment and update schema with composer command

--- src/Acme/ApiBundle/Composer/HerokuEnvironment.php
+++ src/Acme/ApiBundle/Composer/HerokuEnvironment.php
@@ -16,6 +16,7 @@ class HerokuEnvironment
 
         if ($url) {
             $url = parse_url($url);
+            putenv("SYMFONY__DATABASE_DRIVER=pdo_mysql");
             putenv("SYMFONY__DATABASE_HOST={$url['host']}");
             putenv("SYMFONY__DATABASE_USER={$url['user']}");
             putenv("SYMFONY__DATABASE_PASSWORD={$url['pass']}");
@@ -26,5 +27,24 @@ class HerokuEnvironment
 
         $io = $event->getIO();
         $io->write('CLEARDB_DATABASE_URL=' . getenv('CLEARDB_DATABASE_URL'));
+    }
+
+    /**
+     * Update DB schema and load fixtures
+     *
+     * @param Event $event
+     */
+    public static function updateSchema(Event $event)
+    {
+        $options = self::getOptions($event);
+        $consoleDir = self::getConsoleDir($event, 'build');
+
+        if (null === $consoleDir) {
+            return;
+        }
+
+        static::executeCommand($event, $consoleDir, 'doctrine:schema:update --force', $options['process-timeout']);
+        static::executeCommand($event, $consoleDir, 'doctrine:fixtures:load --append', $options['process-timeout']);
+
     }
 }
